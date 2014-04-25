2fe866305bb0d54fc2ccb3131be498d82141be84
ruanyf <yifeng.ruan@gmail.com>
2014-04-26 00:41:11 +0800

    edit ditto.js

--- js/ditto.js
+++ js/ditto.js
@@ -197,7 +197,7 @@ function router() {
 
 	disqus_identifier = path;
 	disqus_title = $(ditto.content_id+" h1").text();
-	disqus_url = path;
+	disqus_url = location.host + '?location='+path;
 
     // otherwise get the markdown and render it
     var loading = show_loading();
