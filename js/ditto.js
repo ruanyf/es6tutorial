2680f4756fd2de9df8ae851800dfe78a46417ab8
ruanyf <yifeng.ruan@gmail.com>
2014-05-01 13:18:22 +0800

    add progress indicator

--- js/ditto.js
+++ js/ditto.js
@@ -258,6 +258,24 @@ function router() {
 			$('#pagedown').css('display','inline-block');
 		}
 
+		(function(){
+			var $w = $(window);
+			var $prog2 = $('.progress-indicator-2');
+			var wh = $w.height();
+			var h = $('body').height();
+			var sHeight = h - wh;
+			$w.off('scroll');
+			$w.on('scroll', function(){
+				var perc = Math.max(0, Math.min(1, $w.scrollTop()/sHeight));
+				updateProgress(perc);
+			});
+
+			function updateProgress(perc){
+				$prog2.css({width : perc*100 + '%'});
+			}
+
+		}());
+
     }).fail(function() {
         show_error();
 
