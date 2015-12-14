c3f4ad4421d2ee1b44a581c54845162eb815fd18
ruanyf <yifeng.ruan@gmail.com>
2015-12-14 21:43:59 +0800

    edit ditto.js

---
+++ config.js
@@ -0,0 +1,26 @@
+var CONFIG = {
+  // your website's title
+  document_title: "ECMAScript 6入门",
+
+  // index page
+  index: "README.md",
+
+  // sidebar file
+  sidebar_file: "sidebar.md",
+
+  // where the docs are actually stored on github - so you can edit
+  base_url: "https://github.com/ruanyf/es6tutorial/edit/gh-pages",
+};
+
+// **************************
+// DON'T EDIT FOLLOWING CODES
+// **************************
+
+addConfig(ditto, CONFIG);
+
+function addConfig(obj, conf) {
+  Object.keys(conf).forEach(function (key) {
+    obj[key] = conf[key];
+  });
+}
+
